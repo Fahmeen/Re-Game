9d339d22e2a1387aa7970b946b4af90ef97abe11
Fahmeen Mazhar <fahmeenmazhar@gmail.com>
2020-07-11 10:05:19 +0500

    Cleaned up code

--- Assets/Scripts/PlayerControllerE.cs
+++ Assets/Scripts/PlayerControllerE.cs
@@ -25,9 +25,7 @@ public class PlayerControllerE : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
         transform.Translate(movement * Time.deltaTime * speed);
     }
 
