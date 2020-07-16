e7deede02cbb1aad4594bdecd69fc08984bfff6b
Fahmeen <fahmeenmazhar@gmail.com>
2020-07-16 12:30:42 +0500

    Unity ReGame

---
+++ KinectReGame/KinectGame/Assets/Scripts/RotatorZ.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatorZ : MonoBehaviour 
+{
+  void Start () 
+    {
+    }
+
+void Update () 
+    {
+        transform.Rotate(new Vector3(360, 360, 360) * Time.deltaTime);
+    }
+}
+
